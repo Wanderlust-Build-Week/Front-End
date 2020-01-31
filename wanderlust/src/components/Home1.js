a47864bff4049fcb8a2b3e1a7321b469854e299d
bobbidigi <bobbidigi34@gmail.com>
2020-01-30 20:22:49 -0500

    makking me commit

--- wanderlust/src/components/Home1.js
+++ wanderlust/src/components/Home1.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home1() {
     return (
         <div>
-            this is a home splahsh page with navigation
+            
         </div>
     )
 }
