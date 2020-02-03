9c73618100c7af5054f43414d4e25a788d5d0f9d
Candace Wilson <candaceyw@gmail.com>
2020-02-02 20:22:14 -0500

    changing directories take 3

--- wanderlust/src/App.js
+++ wanderlust/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import {Route} from 'react-router-dom'
 import {BrowserRouter as Router} from "react-router-dom"
-import TourGuide from './components/TourGuide'
-import NewTourForm from './components/NewTourForm'
-import Login1 from './components/Login1'
-import Register1 from './components/Register1'
-import Home1 from './components/Home1'
+import TourGuide from './Components/TourGuide'
+import NewTourForm from './Components/NewTourForm'
+import Login1 from './Components/Login1'
+import Register1 from './Components/Register1'
+import Home1 from './Components/Home1'
 
 function App(props) {
   console.log("app props", props)
--- wanderlust/src/components/Carousel/SimpleSlider.js
+++ wanderlust/src/components/Carousel/SimpleSlider.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from 'react'
 import img1 from '../../images/img1.jpg'
 import img2 from '../../images/img2.jpg'
 import img3 from '../../images/img3.jpg'
@@ -9,7 +9,7 @@ import './Slider.css';
 import {
   Carousel,
   CarouselItem,
-  } from 'reactstrap';
+  } from './node_modules/reactstrap';
 
 const items = [
   {
