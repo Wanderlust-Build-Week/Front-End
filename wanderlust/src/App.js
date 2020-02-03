f4c7812fe1d0baf354278623831a3aae0ed402b1
Candace Wilson <candaceyw@gmail.com>
2020-02-02 20:32:29 -0500

    take 4 dir issues

--- wanderlust/src/App.js
+++ wanderlust/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import {Route} from 'react-router-dom'
 import {BrowserRouter as Router} from "react-router-dom"
-import TourGuide from './Components/TourGuide'
-import NewTourForm from './Components/NewTourForm'
-import Login1 from './Components/Login1'
-import Register1 from './Components/Register1'
-import Home1 from './Components/Home1'
+import TourGuide from './temp-components/TourGuide'
+import NewTourForm from './temp-components/NewTourForm'
+import Login1 from './temp-components/Login1'
+import Register1 from './temp-components/Register1'
+import Home1 from './temp-components/Home1'
 
 function App(props) {
   console.log("app props", props)
