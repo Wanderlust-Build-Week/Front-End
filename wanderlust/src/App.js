993556298b7e7847c056588c4cd669e0460843f7
bobbidigi <bobbidigi34@gmail.com>
2020-02-03 22:02:22 -0500

    fix merge conflict

--- wanderlust/src/App.js
+++ wanderlust/src/App.js
@@ -4,15 +4,10 @@ import {Route} from 'react-router-dom'
 import {BrowserRouter as Router} from "react-router-dom"
 import TourGuide from './components/TourGuide'
 import NewTourForm from './components/NewTourForm'
-import Login1 from './components/Login1'
+import Login from './components/Login'
 import Register1 from './components/Register1'
 import Home1 from './components/Home1'
-<<<<<<< HEAD
-import FormikContactForm from './components/Contact'
-
-=======
 import User from './components/User'
->>>>>>> b6a0c8a6c6f9323770daa0a130efd51cc8bdae9e
 function App(props) {
   console.log("app props", props)
 
@@ -21,18 +16,12 @@ function App(props) {
       <div className="App">
         <Route exact path="/" render={props =><Home1 {...props}/>}/>
         <Route path="/register" render={ props =><Register1 {...props}/>}/>
-        <Route path="/login" render={ props =><Login1 {...props}/>}/>
+        <Route path="/login" render={ props =><Login {...props}/>}/>
         <Route path="/tourguide/:id" component={TourGuide}/>
         <Route path="/tourguide/newTourForm" component={NewTourForm}/>
-<<<<<<< HEAD
-        <Route path="/contact" render={ props =><FormikContactForm {...props}/>}/>
-
-=======
         <Route path="/user" component={User}/>
->>>>>>> b6a0c8a6c6f9323770daa0a130efd51cc8bdae9e
       </div>
  
-    
   );
 }
 
