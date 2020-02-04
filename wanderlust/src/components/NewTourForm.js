477f76faa39878c2bab08026604e9a28e6208d2f
Michael Trew <mtrew2015@gmail.com>
2020-02-04 00:38:33 -0500

    successfully troubleshooted Formik Component

--- wanderlust/src/components/NewTourForm.js
+++ wanderlust/src/components/NewTourForm.js
@@ -1,34 +1,55 @@
-import React, { useState, useEffect } from "react";
-import { Form, Field, withFormik } from "formik";
-import * as Yup from "yup";
-import axios from "axios";
-
+import React, { useState, useEffect } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
 //Declaring my form values/wireframe w/ the Formik component
-const NewTourForm = ({ errors, touched, values, status }) => {
-    const [form, setForm] = useState([]);
+const NewTourForm = () => {
+	const [ form, setForm ] = useState([]);
 
-    return(
-        <div className="new-tour">
-            <h1>Book a New Tour!</h1>
-    {/*Step 1: Set up the fields of the form*/}
-                <Field
-                type="text"
-                name="title"
-                placeholder="Book your new tour!"
-                />
-
-                {/* <Field
-                type="text"
-                name="location"
-                placeholder="Enter Location Here"
-                /> */}
-        </div>
-    );
+	return (
+		<div className='new-tour'>
+			<h1>Book a New Tour!</h1>
+			{/*Step 1: Set up the fields of the form*/}
+			<Formik
+				initialValues={{ title: '', location: '', duration: 0, tourStyle:'' }}
+				validate={(values) => {
+					const errors = {};
+					if (!values.title) {
+                        errors.title = 'Title Is Required';
+                    }
+                    if(!values.location){
+                        errors.location = "Location is Required"
+                    }
+					return errors;
+				}}
+				onSubmit={(values, { setSubmitting }) => {
+					setTimeout(() => {
+                        // axios post will go here
+						console.log(values);
+						setSubmitting(false);
+					}, 400);
+				}}>
+				{({ isSubmitting }) => {
+					return (
+						<Form>
+							<Field placeholder='title' name='title' />
+							<Field placeholder='location' name='location' />
+							<Field placeholder='duration' type="number" name='duration' />
+							<Field name="tourStyle" component='select'>
+								<option>Professional</option>
+								<option>Private</option>
+							</Field>
+							<ErrorMessage name='title' component='div' />
+							<button type='submit' disabled={isSubmitting}>
+								Submit
+							</button>
+						</Form>
+					);
+				}}
+			</Formik>
+		</div>
+	);
 };
 
-//Higher Order Component
-const FormikNewTourForm = withFormik({})(NewTourForm);
-
 export default NewTourForm;
-
