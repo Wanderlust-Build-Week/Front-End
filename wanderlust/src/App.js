36c52c936998e8842bb512eee5934a201079e46e
Joshua Vandergriff <vanditechnician@gmail.com>
2020-02-04 22:46:53 -0600

    set up the HOC and the default states within Formik.

--- wanderlust/src/App.js
+++ wanderlust/src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import {Route} from 'react-router-dom'
 import {BrowserRouter as Router} from "react-router-dom"
 import TourGuide from './components/TourGuide'
-import NewTourForm from './components/NewTourForm'
 import Login1 from './components/Login1'
 import Register1 from './components/Register1'
 import Home1 from './components/Home1'
+import FormikNewTourForm from './components/NewTourForm';
 
 
 function App(props) {
@@ -20,7 +20,7 @@ function App(props) {
         <Route path="/login" render={ props =><Login1 {...props}/>}/>
         <Route path="/tourguide/:id" component={TourGuide}/>
         <Route path="/tourguide/newTourForm" component={NewTourForm}/> */}
-        <Route path="/" component={NewTourForm}/>
+        <FormikNewTourForm start = "Location"/>
       </div>
  
     
--- wanderlust/src/components/NewTourForm.js
+++ wanderlust/src/components/NewTourForm.js
@@ -16,11 +16,19 @@ const NewTourForm = ({ errors, touched, values, status }) => {
           placeholder="Tour Title Here"
         />
 
-        <Field 
-        type="text" 
-        name="location" 
-        placeholder="Location Here"  
+        <Field
+          type="text"
+          name="start"
+          placeholder="Location Here"
+          value={values.start}
         />
+
+        {/* <Field 
+        type="text" 
+        name="location" // <----- Did NOT like the word "Location" being used..? (would render Object object
+        on the form)
+        placeholder="Location Here"
+        /> */}
 
         <Field
           component="textarea"
@@ -33,15 +41,16 @@ const NewTourForm = ({ errors, touched, values, status }) => {
         placeholder='# of Days' 
         type="number" 
         name='duration' 
+        placeholder="0"
         />
 
-        <Field component="select" name="food">
+        <Field component="select" name="options">
           <option>Choose an option</option>
           <option>Private</option>
           <option>Professional</option>
         </Field>
 
-        <button type="submit">Submit</button>
+        <button type="submit">Enter</button>
       </Form>
 
     </div>
@@ -49,6 +58,16 @@ const NewTourForm = ({ errors, touched, values, status }) => {
 };
 
 // higher order component
-const FormikNewTourForm = withFormik({NewTourForm});
+const FormikNewTourForm = withFormik({
+    mapPropsToValues({ start }) {
+        return {
+            title: "",
+            start: start,
+            body: "",
+            duration: "",
+            options: ""
+        };
+    }
+})(NewTourForm);
 
 export default FormikNewTourForm;
