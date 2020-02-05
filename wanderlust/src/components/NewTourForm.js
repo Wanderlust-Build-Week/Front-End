9062533d81e2b4fd69bcadebd4fccb980722dd77
bobbidigi <bobbidigi34@gmail.com>
2020-02-05 01:34:05 -0500

    add joshs to dev

--- wanderlust/src/components/NewTourForm.js
+++ wanderlust/src/components/NewTourForm.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
-
+// test
 const NewTourForm = ({ errors, touched, values, status }) => {
   const [forms , setForms] = useState([]);
 
